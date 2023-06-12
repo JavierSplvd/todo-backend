b03972f48865367c6aed99fc746255a980b61e4b
javier <javier.sepulveda.verdugo@gmail.com>
2023-06-12 20:06:11 +0200

    feat: update seed files

--- db/1_init.sql
+++ db/1_init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE items (
     id VARCHAR(255) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
+    done BOOLEAN NOT NULL DEFAULT false,
     updated_at TIMESTAMP DEFAULT NOW(),
     created_at TIMESTAMP DEFAULT NOW()
 );
--- db/2_seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO items (title) VALUES ('Item 1');
-INSERT INTO items (title) VALUES ('Item 2');
-INSERT INTO items (title) VALUES ('Item 3');
